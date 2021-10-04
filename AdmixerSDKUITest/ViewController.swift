bb72097ee33dc63d538d3137f43f1e2e76d2aff5
icq9 <mihanik844@gmail.com>
2021-10-04 14:14:16 +0300

    Initial Commit

---
+++ AdmixerSDKUITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdmixerSDKUITest
+//
+//  Created by MacBookPro on 04.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
